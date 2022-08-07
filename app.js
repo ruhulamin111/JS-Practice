b22626b409afbc7fe49af8bcf06d5541956329ed
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-07 10:33:54 +0600

    add every method check

--- app.js
+++ app.js
@@ -8,3 +8,19 @@ function myFunction() {
     document.getElementById("demo1").innerHTML =
         text.replace("Microsoft", "W3Schools");
 }
+
+let price = 100;
+let VAT = 0.25;
+let total = `Total: ${(price * (1 + VAT)).toFixed(2)}`;
+
+
+document.getElementById("demo2").innerHTML = total;
+
+const numbers = [45, 20, 229, 35, 25];
+let allOver18 = numbers.every(myFunction);
+
+document.getElementById("demo3").innerHTML = "All over 18 is " + allOver18;
+
+function myFunction(value, index, array) {
+    return value > 18;
+}
