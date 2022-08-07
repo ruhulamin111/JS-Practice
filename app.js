361911d70753fef32cf2be4f00554c8d16967905
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-07 16:41:58 +0600

    add date and switch condition update

--- app.js
+++ app.js
@@ -47,11 +47,38 @@ document.getElementById("demo8").innerHTML = Math.trunc(12.67);
 function random() {
     document.getElementById("demo9").innerHTML = Math.random().toFixed(2) * 100;
 }
-
-
+// 10
 function myFunction() {
     let age = document.getElementById("age").value;
     let voteable = (age < 18) ? "Too young" : "Old enough";
     document.getElementById("demo10").innerHTML = voteable + " to vote.";
 }
+// 11
+let day;
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+}
+document.getElementById("demo11").innerHTML = "Today is " + day;
+
+
+
 
